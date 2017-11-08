9cd45efb027c4d14ce610bde1f2e67f141740aef
prabh961 <prabh961@gmail.com>
2017-11-08 15:39:58 -0500

    raj

--- public/assets/js/barList.js
+++ public/assets/js/barList.js
@@ -1,3 +1,20 @@
+'use strict';
+
+var serverUrl = 'http://localhost:3030';
+const feathersClient = feathers()
+  .configure(feathers.rest(serverUrl).fetch(fetch));
+
+const bars = feathersClient.service('/bars');
+
+bars.find({
+    query: {
+      id: {
+        $ne: -1
+      },
+      $limit: 1000
+  }
+}).then(results => console.log('All results\n', results));
+
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
   $scope.id = 0;
