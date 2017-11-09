8397360185059ca7b5b6bc107961df08e097e038
apac <apac>
2017-11-08 22:07:23 -0500

    added drinker service

---
+++ src/models/drinkers.model.js
@@ -0,0 +1,99 @@
+'use strict';
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const drinkers = sequelizeClient.define('drinkers', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      get() {
+        return this.getDataValue('id');
+      },
+      set(drinkerId) {
+        this.setDataValue('id', drinkerId);
+      }
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      get() {
+        return this.getDataValue('name');
+      },
+      set(drinkerName) {
+        this.setDataValue('name', drinkerName);
+      }
+    },
+    gender: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      get() {
+        return this.getDataValue('gender');
+      },
+      set(drinkerGender) {
+        this.setDataValue('gender', drinkerGender);
+      }
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      get() {
+        return this.getDataValue('age');
+      },
+      set(drinkerAge) {
+        this.setDataValue('age', drinkerAge);
+      }
+    },
+    city: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      get() {
+        return this.getDataValue('city');
+      },
+      set(drinkerCity) {
+        this.setDataValue('city', drinkerCity);
+      }
+    },
+    state: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      get() {
+        return this.getDataValue('state');
+      },
+      set(drinkerState) {
+        this.setDataValue('state', drinkerState);
+      }
+    },
+    zipcode: {
+      type: DataTypes.INTEGER(5).ZEROFILL,
+      validate: {
+        len: [5, 5]
+      },
+      allowNull: false,
+      get() {
+        return this.getDataValue('zipcode');
+      },
+      set(zipcode) {
+        this.setDataValue('zipcode', zipcode);
+      }
+    }
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  drinkers.associate = function (models) { // eslint-disable-line no-unused-vars
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return drinkers;
+};
---
+++ src/services/drinkers/drinkers.service.js
@@ -0,0 +1,29 @@
+// Initializes the `drinkers` service on path `/drinkers`
+const createService = require('feathers-sequelize');
+const createModel = require('../../models/drinkers.model');
+const hooks = require('./drinkers.hooks');
+const filters = require('./drinkers.filters');
+
+module.exports = function () {
+  const app = this;
+  const Model = createModel(app);
+  const paginate = app.get('paginate');
+
+  const options = {
+    name: 'drinkers',
+    Model,
+    paginate
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/drinkers', createService(options));
+
+  // Get our initialized service so that we can register hooks and filters
+  const service = app.service('drinkers');
+
+  service.hooks(hooks);
+
+  if (service.filter) {
+    service.filter(filters);
+  }
+};
