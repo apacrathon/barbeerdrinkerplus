13c97c209589ffe604cc95de18a8311b28d695cf
apac <apac>
2017-11-08 22:09:11 -0500

    be saved.

--- public/assets/js/barList.js
+++ public/assets/js/barList.js
@@ -1,26 +1,24 @@
 'use strict';
 
-var serverUrl = 'http://localhost:3030';
+let serverUrl = 'http://localhost:3030';
 const feathersClient = feathers()
   .configure(feathers.rest(serverUrl).fetch(fetch));
 
 const bars = feathersClient.service('/bars');
 
-bars.find({
-    query: {
-      id: {
-        $ne: -1
-      },
-      $limit: 1000
-  }
-}).then(results => console.log('All results\n', results));
+let app = angular.module('myApp', []);
 
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-  $scope.id = 0;
-  $scope.barName = "Evil Roy Slades";
-  $scope.barStreet = "7926 Nichols Avenue";
-  $scope.barCity = "Austin";
-  $scope.barState = "TX";
-  $scope.barZipCode = "78730";
-})
+app.controller('myCtrl', [
+  '$scope',
+  function($scope) {
+    $scope.barList = [];
+    bars.find({
+      query: { id: { $ne: -1 }, $limit: 1000 }
+    }).then(function(response) {
+      $scope.$apply(() => {
+        $scope.barList = response.data;
+      });
+    });
+    console.log($scope.barList);
+  }
+]);
