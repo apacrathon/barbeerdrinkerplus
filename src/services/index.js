ac6824a1957083df43adfa4e98aaef3352baddf1
apacrathon <raj.patel1096@gmail.com>
2017-11-13 20:28:01 -0500

    added ratings

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const bars = require('./bars/bars.service.js');
 const users = require('./users/users.service.js');
+const ratings = require('./ratings/ratings.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(bars);
   app.configure(users);
+  app.configure(ratings);
 };
