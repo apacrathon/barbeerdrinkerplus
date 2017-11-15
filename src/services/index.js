cb7e6f442ea5fe2cddf44bff8afd41a81466ac79
apacrathon <raj.patel1096@gmail.com>
2017-11-15 16:03:46 -0500

    integrates happyhour service

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const bars = require('./bars/bars.service.js');
 const users = require('./users/users.service.js');
 const ratings = require('./ratings/ratings.service.js');
 const sells = require('./sells/sells.service.js');
+const happyhour = require('./happyhour/happyhour.service.js');
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(bars);
   app.configure(users);
   app.configure(ratings);
   app.configure(sells);
+  app.configure(happyhour);
 };
