1241e7a349b1ebb71637cfa20c2ff0beff5594c9
apacrathon <raj.patel1096@gmail.com>
2017-11-18 18:25:39 -0500

    define likes and FKs

--- src/models/likes.model.js
+++ src/models/likes.model.js
@@ -6,21 +6,51 @@ const DataTypes = Sequelize.DataTypes;
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
   const likes = sequelizeClient.define('likes', {
-    text: {
+    drinkerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      get() {
+        return this.getDataValue('drinkerId');
+      },
+      set(drinkerId) {
+        this.setDataValue('drinkerId', drinkerId);
+      }
+    },
+    drinkerName: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      get() {
+        return this.getDataValue('drinkerName');
+      },
+      set(drinkerName) {
+        this.setDataValue('drinkerName', drinkerName);
+      }
+    },
+    drinkName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      get() {
+        return this.getDataValue('drinkName');
+      },
+      set(drinkName) {
+        this.setDataValue('drinkName', drinkName);
+      }
     }
+  }, {
+    timestamps: false
   }, {
     hooks: {
       beforeCount(options) {
         options.raw = true;
       }
     }
-  });
+});
 
   likes.associate = function (models) { // eslint-disable-line no-unused-vars
-    // Define associations here
-    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+    likes.belongsTo(models.drinkers, { foreignKey: 'drinkerId' });
+    likes.belongsTo(models.drink, { foreignKey: 'drinkName' });
   };
 
   return likes;
