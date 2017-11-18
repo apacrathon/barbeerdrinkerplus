4a68d922a6a92935af75d20032c09814cd344ef6
apacrathon <raj.patel1096@gmail.com>
2017-11-18 18:39:17 -0500

    define model bartimes + FK

--- src/models/bartimes.model.js
+++ src/models/bartimes.model.js
@@ -1,15 +1,64 @@
-// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
-// for more of what you can do here.
+'use strict';
+
 const Sequelize = require('sequelize');
 const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
   const bartimes = sequelizeClient.define('bartimes', {
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false
+    barId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      get() {
+        return this.getDataValue('barId');
+      },
+      set(barId) {
+        this.setDataValue('barId', barId);
+      }
+    },
+    day: {
+      type: DataTypes.STRING.BINARY,
+      allowNull: false,
+      get() {
+        return this.getDataValue('day');
+      },
+      set(day) {
+        this.setDataValue('day', day);
+      },
+      validate: {
+        is: /^[01]+$/, // Binary regex
+        len: [7,7]
+      }
+    },
+    openTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+      validate: {
+        is: /^([01]\d|2[0-3]):?([0-5]\d)$/   // 24 hour time regex
+      },
+      get() {
+        return this.getDataValue('openTime');
+      },
+      set(openTime) {
+        this.setDataValue('openTime', openTime);
+      }
+    },
+    closeTime: {
+      type: DataTypes.TIME,
+      allowNull: false,
+      validate: {
+        is: /^([01]\d|2[0-3]):?([0-5]\d)$/  // 24 hour time regex
+      },
+      get() {
+        return this.getDataValue('closeTime');
+      },
+      set(closeTime) {
+        this.setDataValue('closeTime', closeTime);
+      }
     }
+  }, {
+    timestamps: false
   }, {
     hooks: {
       beforeCount(options) {
@@ -19,8 +68,7 @@ module.exports = function (app) {
   });
 
   bartimes.associate = function (models) { // eslint-disable-line no-unused-vars
-    // Define associations here
-    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+    bartimes.belongsTo(models.bars, { foreignKey: 'barId' });
   };
 
   return bartimes;
