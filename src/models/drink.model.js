9e7cb9318db050862327a33a9e70a2485d6e3917
apacrathon <raj.patel1096@gmail.com>
2017-11-18 18:14:01 -0500

    define model @TODO: add FKs for dependencies

--- src/models/drink.model.js
+++ src/models/drink.model.js
@@ -1,22 +1,51 @@
-// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
-// for more of what you can do here.
+'use strict';
+
 const Sequelize = require('sequelize');
 const DataTypes = Sequelize.DataTypes;
 
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
   const drink = sequelizeClient.define('drink', {
-    text: {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      get() {
+        this.getDataValue('id');
+      },
+      set(id) {
+        this.setDataValue('id', id);
+      }
+    },
+    name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      get() {
+        this.getDataValue('name');
+      },
+      set(name) {
+        this.setDataValue('name', name);
+      }
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      get() {
+        this.getDataValue('type');
+      },
+      set(type) {
+        this.setDataValue('type', type);
+      }
     }
+  }, {
+    timestamps: false
   }, {
     hooks: {
       beforeCount(options) {
         options.raw = true;
       }
     }
-  });
+});
 
   drink.associate = function (models) { // eslint-disable-line no-unused-vars
     // Define associations here
