fb1f48cdc472555fe05377f8128296d1e6fde125
apacrathon <raj.patel1096@gmail.com>
2017-11-18 18:46:01 -0500

    add FK for drinkName on sells

--- src/models/drink.model.js
+++ src/models/drink.model.js
@@ -50,6 +50,7 @@ module.exports = function (app) {
 
   drink.associate = function (models) { // eslint-disable-line no-unused-vars
     drink.hasMany(models.likes, { foreignKey: 'drinkName' });
+    drink.hasMany(models.sells, { foreignKey: 'drinkName' });
   };
 
   return drink;
