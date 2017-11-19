56cdb4e4c4d3d823af712f64c8bc1ab656101cec
apacrathon <raj.patel1096@gmail.com>
2017-11-19 04:42:49 -0500

    https stuff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@
 const logger = require('winston');
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const fs = require('fs');
+const https  = require('https');
+//const server = app.listen(port);
+const server = https.createServer({
+  key: fs.readFileSync('privateKey.key'),
+  cert: fs.readFileSync('certificate.crt')
+}, app).listen(443);
+
+app.setup(server);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
