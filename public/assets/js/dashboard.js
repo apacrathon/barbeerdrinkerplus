3e223bf1123d1bf4c49bb231adfde2f419c509b1
prabh961 <prabh961@gmail.com>
2017-11-20 21:45:21 -0500

    Faster changes

--- public/assets/js/dashboard.js
+++ public/assets/js/dashboard.js
@@ -117,172 +117,242 @@ managerApp.controller('managerGraphs', [
         }
       );
 
-      ratings.find({
+      // ratings.find({
+      //   query: {
+      //     barId: barValue,
+      //     $limit: 1000
+      //   }
+      // }).then(
+      //   function(response) {
+      //     $scope.$apply(() => {
+      //       $scope.barData = response.data;
+      //       var i = 0;
+      //       var ratingDay = [];
+      //       var ratingNum = [];
+      //
+      //
+      //       //console.log($scope.barData);
+      //       for(i = 0; i < $scope.barData.length; i++) {
+      //         var dateList = $scope.barData[i].dateTime;
+      //         var newDate = new Date(dateList);
+      //         var sFullYear = newDate.getFullYear();
+      //         var sMonth = newDate.getMonth()+1;
+      //         var sDate = newDate.getDate();
+      //         var sHours = newDate.getHours();
+      //         var sMinutes = newDate.getMinutes();
+      //         var sSeconds = newDate.getSeconds();
+      //         if (sMonth < 10) sMonth = "0" + sMonth;
+      //         if (sHours < 10) sHours = "0" + sHours;
+      //         if (sDate < 10) sDate = "0" + sDate;
+      //         if (sMinutes < 10) sMinutes = "0" + sMinutes;
+      //         if (sSeconds < 10) sSeconds = "0" + sSeconds;
+      //         var ratingDate = sFullYear+'-'+sMonth+'-'+sDate+' '+sHours+':'+sMinutes+':'+sSeconds;
+      //         ratingDay.push(ratingDate);
+      //         ratingNum.push($scope.barData[i].rating);
+      //         //ratingSum += $scope.barData[i].rating;
+      //         let ratingSum = 0;
+      //         let m;
+      //         for(m = 0; m < i+1; m++) {
+      //           ratingSum += $scope.barData[m].rating;
+      //         }
+      //         $scope.barData[i].ratingAvgAtPoint = (ratingSum/m);
+      //         $scope.barData[i].ratingDay = ratingDay[i].toString();
+      //       }
+      //       var ratingAvgArr = [];
+      //       for(i = 0; i < $scope.barData.length; i++) {
+      //         ratingAvgArr.push($scope.barData[i].ratingAvgAtPoint);
+      //       }
+      //       //console.log(ratingAvgArr);
+      //       var data = [
+      //         {
+      //           x: ratingDay,
+      //           y: ratingNum,
+      //           type: 'scatter',
+      //           mode: "lines",
+      //           name: 'Rating'
+      //         },
+      //         {
+      //           x: ratingDay,
+      //           y: ratingAvgArr,
+      //           type: 'scatter',
+      //           mode: "lines",
+      //           name: 'Average'
+      //         }
+      //       ];
+      //
+      //       var layout =
+      //         {
+      //           title: 'Ratings for '+ $scope.barData[0].barName,
+      //           titlefont: {
+      //             size: 28,
+      //             family: 'Raleway, sans-serif'
+      //           },
+      //           xaxis: {
+      //             title: 'Time',
+      //             autorange: true,
+      //             rangeslider: { range: [ratingDay[0], ratingDay[ratingDay.length-1]] },
+      //             type: 'date'
+      //           },
+      //           yaxis: {
+      //             title: 'Rating',
+      //             autorate: true,
+      //             range: [0,6],
+      //             type: 'linear'
+      //           }
+      //         }
+      //         //console.log($scope.barData[5]);
+      //       Plotly.newPlot('myDiv', data, layout);
+      //       //console.log($scope.barData[0].drinkerId);
+      //       for(i = 0; i < $scope.barData.length; i++) {
+      //         drinkers.find({
+      //           query: {
+      //             id: $scope.barData[i].drinkerId,
+      //           }
+      //         }).then(
+      //           function (response3) {
+      //             $scope.$apply(() => {
+      //               //console.log(i + " ID " + response3.data[0].id + " " + response3.data[0].name);
+      //               for(i = 0; i < $scope.barData.length; i++) {
+      //                 if($scope.barData[i].drinkerId == response3.data[0].id) {
+      //                   $scope.barData[i].drinkerName = response3.data[0].name;
+      //                 }
+      //               }
+      //             });
+      //           });
+      //       }
+      //       console.log($scope.barData);
+      //       $scope.ratingsTable = new NgTableParams({
+      //         page: 1,
+      //         count: 15
+      //       }, {
+      //         total: $scope.barData.length,
+      //         getData: function (params) {
+      //           $scope.data = params.sorting() ? $filter('orderBy')($scope.barData, params.orderBy()) : $scope.barData;
+      //           $scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
+      //           $scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
+      //           return $scope.data;
+      //         }
+      //       });
+      //     });
+      //   });
+
+      query.find({
         query: {
-          barId: barValue,
-          $limit: 1000
+          rawQuery: "SELECT * FROM test.ratings R, test.drinkers D WHERE R.barId = "+barValue+" AND D.id = R.drinkerId ORDER BY dateTime ASC;"
         }
-      }).then(
-        function(response) {
-          $scope.$apply(() => {
-            $scope.barData = response.data;
-            var i = 0;
-            var ratingDay = [];
-            var ratingNum = [];
-            $scope.barData.sort(
-              function(a,b) {
-                var dateA = new Date(a.dateTime);
-                var dateB = new Date(b.dateTime);
-
-                return dateA - dateB;
-              }
-            );
-
-            //console.log($scope.barData);
-            for(i = 0; i < $scope.barData.length; i++) {
-              var dateList = $scope.barData[i].dateTime;
-              var newDate = new Date(dateList);
-              var sFullYear = newDate.getFullYear();
-              var sMonth = newDate.getMonth()+1;
-              var sDate = newDate.getDate();
-              var sHours = newDate.getHours();
-              var sMinutes = newDate.getMinutes();
-              var sSeconds = newDate.getSeconds();
-              if (sMonth < 10) sMonth = "0" + sMonth;
-              if (sHours < 10) sHours = "0" + sHours;
-              if (sDate < 10) sDate = "0" + sDate;
-              if (sMinutes < 10) sMinutes = "0" + sMinutes;
-              if (sSeconds < 10) sSeconds = "0" + sSeconds;
-              var ratingDate = sFullYear+'-'+sMonth+'-'+sDate+' '+sHours+':'+sMinutes+':'+sSeconds;
-              ratingDay.push(ratingDate);
-              ratingNum.push($scope.barData[i].rating);
-              //ratingSum += $scope.barData[i].rating;
-              let ratingSum = 0;
-              let m;
-              for(m = 0; m < i+1; m++) {
-                ratingSum += $scope.barData[m].rating;
-              }
-              $scope.barData[i].ratingAvgAtPoint = (ratingSum/m);
-              $scope.barData[i].ratingDay = ratingDay[i].toString();
+      }).then(response => {
+        $scope.$apply(() => {
+          console.log(response[0]);
+          $scope.barData = response[0];
+          var i = 0;
+          var ratingDay = [];
+          var ratingNum = [];
+          for(i = 0; i < $scope.barData.length; i++) {
+            var dateList = $scope.barData[i].dateTime;
+            var newDate = new Date(dateList);
+            var sFullYear = newDate.getFullYear();
+            var sMonth = newDate.getMonth()+1;
+            var sDate = newDate.getDate();
+            var sHours = newDate.getHours();
+            var sMinutes = newDate.getMinutes();
+            var sSeconds = newDate.getSeconds();
+            if (sMonth < 10) sMonth = "0" + sMonth;
+            if (sHours < 10) sHours = "0" + sHours;
+            if (sDate < 10) sDate = "0" + sDate;
+            if (sMinutes < 10) sMinutes = "0" + sMinutes;
+            if (sSeconds < 10) sSeconds = "0" + sSeconds;
+            var ratingDate = sFullYear+'-'+sMonth+'-'+sDate+' '+sHours+':'+sMinutes+':'+sSeconds;
+            ratingDay.push(ratingDate);
+            ratingNum.push($scope.barData[i].rating);
+            //ratingSum += $scope.barData[i].rating;
+            let ratingSum = 0;
+            let m;
+            for(m = 0; m < i+1; m++) {
+              ratingSum += $scope.barData[m].rating;
             }
-            var ratingAvgArr = [];
-            for(i = 0; i < $scope.barData.length; i++) {
-              ratingAvgArr.push($scope.barData[i].ratingAvgAtPoint);
+            $scope.barData[i].ratingAvgAtPoint = (ratingSum/m);
+            $scope.barData[i].ratingDay = ratingDay[i].toString();
+          }
+          var ratingAvgArr = [];
+          for(i = 0; i < $scope.barData.length; i++) {
+            ratingAvgArr.push($scope.barData[i].ratingAvgAtPoint);
+          }
+          //console.log(ratingAvgArr);
+          var data = [
+            {
+              x: ratingDay,
+              y: ratingNum,
+              type: 'scatter',
+              mode: "lines",
+              name: 'Rating'
+            },
+            {
+              x: ratingDay,
+              y: ratingAvgArr,
+              type: 'scatter',
+              mode: "lines",
+              name: 'Average'
             }
-            //console.log(ratingAvgArr);
-            var data = [
-              {
-                x: ratingDay,
-                y: ratingNum,
-                type: 'scatter',
-                mode: "lines",
-                name: 'Rating'
+          ];
+
+          var layout =
+            {
+              title: 'Ratings for '+ $scope.barData[0].barName,
+              titlefont: {
+                size: 28,
+                family: 'Raleway, sans-serif'
               },
-              {
-                x: ratingDay,
-                y: ratingAvgArr,
-                type: 'scatter',
-                mode: "lines",
-                name: 'Average'
+              xaxis: {
+                title: 'Time',
+                autorange: true,
+                rangeslider: { range: [ratingDay[0], ratingDay[ratingDay.length-1]] },
+                type: 'date'
+              },
+              yaxis: {
+                title: 'Rating',
+                autorate: true,
+                range: [0,6],
+                type: 'linear'
               }
-            ];
-
-            var layout =
-              {
-                title: 'Ratings for '+ $scope.barData[0].barName,
-                titlefont: {
-                  size: 28,
-                  family: 'Raleway, sans-serif'
-                },
-                xaxis: {
-                  title: 'Time',
-                  autorange: true,
-                  rangeslider: { range: [ratingDay[0], ratingDay[ratingDay.length-1]] },
-                  type: 'date'
-                },
-                yaxis: {
-                  title: 'Rating',
-                  autorate: true,
-                  range: [0,6],
-                  type: 'linear'
-                }
-              }
-              //console.log($scope.barData[5]);
-            Plotly.newPlot('myDiv', data, layout);
-            //console.log($scope.barData[0].drinkerId);
-            for(i = 0; i < $scope.barData.length; i++) {
-              drinkers.find({
-                query: {
-                  id: $scope.barData[i].drinkerId,
-                }
-              }).then(
-                function (response3) {
-                  $scope.$apply(() => {
-                    //console.log(i + " ID " + response3.data[0].id + " " + response3.data[0].name);
-                    for(i = 0; i < $scope.barData.length; i++) {
-                      if($scope.barData[i].drinkerId == response3.data[0].id) {
-                        $scope.barData[i].drinkerName = response3.data[0].name;
-                      }
-                    }
-                  });
-                });
             }
-            console.log($scope.barData);
-            $scope.ratingsTable = new NgTableParams({
-              page: 1,
-              count: 15
-            }, {
-              total: $scope.barData.length,
-              getData: function (params) {
-                $scope.data = params.sorting() ? $filter('orderBy')($scope.barData, params.orderBy()) : $scope.barData;
-                $scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
-                $scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                return $scope.data;
-              }
-            });
+          //console.log($scope.barData[5]);
+          Plotly.newPlot('myDiv', data, layout);
+          $scope.ratingsTable = new NgTableParams({
+            page: 1,
+            count: 15
+          }, {
+            total: $scope.barData.length,
+            getData: function (params) {
+              $scope.data = params.sorting() ? $filter('orderBy')($scope.barData, params.orderBy()) : $scope.barData;
+              $scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
+              $scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
+              return $scope.data;
+            }
           });
         });
-      frequents.find({
+      });
+
+      query.find({
         query: {
-          barId: barValue,
-          $limit: 1000
+          rawQuery: "SELECT * FROM test.drinkers D, test.frequents F WHERE barId = "+barValue+" AND D.id = F.drinkerId;"
         }
-      }).then(
-        function(response) {
-          $scope.$apply(() => {
-            $scope.frequentsList = response.data;
-            $scope.frequentingDrinkers = [];
-            let i;
-            for(i = 0; i < $scope.frequentsList.length-1; i++) {
-              drinkers.find({
-                query: {
-                  id: $scope.frequentsList[i].drinkerId,
-                }
-              }).then(
-                function(response) {
-                  $scope.$apply(() => {
-
-
-                    //console.log(response.data[0]);
-                    $scope.frequentingDrinkers.push(response.data[0]);
-                    $scope.frequentsTable = new NgTableParams({
-                      page: 1,
-                      count: 15
-                    }, {
-                      total: $scope.frequentingDrinkers.length,
-                      getData: function (params) {
-                        $scope.data1 = params.sorting() ? $filter('orderBy')($scope.frequentingDrinkers, params.orderBy()) : $scope.frequentingDrinkers;
-                        $scope.data1 = params.filter() ? $filter('filter')($scope.data1, params.filter()) : $scope.data1;
-                        $scope.data1 = $scope.data1.slice((params.page() - 1) * params.count(), params.page() * params.count());
-                        return $scope.data1;
-                      }
-                    });
-                  });
-                });
+      }).then(response => {
+        $scope.$apply(() => {
+          $scope.frequentingDrinkers = response[0];
+          $scope.frequentsTable = new NgTableParams({
+            page: 1,
+            count: 15
+          }, {
+            total: $scope.frequentingDrinkers.length,
+            getData: function (params) {
+              $scope.data1 = params.sorting() ? $filter('orderBy')($scope.frequentingDrinkers, params.orderBy()) : $scope.frequentingDrinkers;
+              $scope.data1 = params.filter() ? $filter('filter')($scope.data1, params.filter()) : $scope.data1;
+              $scope.data1 = $scope.data1.slice((params.page() - 1) * params.count(), params.page() * params.count());
+              return $scope.data1;
             }
-            console.log($scope.frequentingDrinkers.length);
           });
-        });
+        })
+      });
       query.find({
         query: {
           rawQuery: "SELECT L.drinkName, " +
